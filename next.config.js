e03b868ecb65a674f36c3832773c28ab1d9b7895
Luc Van Kerkvoort <luc.vankerkvoort@quicken.com>
2023-08-05 16:41:29 -0700

    fixed image loader

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   export: "output",
-  images: {
-    loader: "imgix",
-    path: "hllogistics-6531.imgix.net",
-  },
 };
 
 module.exports = nextConfig;
