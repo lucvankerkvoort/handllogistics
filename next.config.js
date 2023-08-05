af48ca5de4b23ba175bc720a22449efc757ec06e
Luc Van Kerkvoort <luc.vankerkvoort@quicken.com>
2023-08-05 16:17:55 -0700

    refined config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,11 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "/";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     export: 'output'
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: "imgix",
-    path: 'the "domain" of your Imigix source',
-  },
 };
+
+// Remove or comment out the "images" property
+nextConfig.images = {
+  ...
+};
+
+module.exports = nextConfig;
